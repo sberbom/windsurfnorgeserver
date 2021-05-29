2bd66eef72fa257ea09c33b3206f03257569632d
sberbom <sigmunbb@stud.ntnu.no>
2021-05-29 21:56:28 +0200

    added ts

---
+++ types.ts
@@ -0,0 +1,17 @@
+type Spot = {
+    id: string;
+    name: string;
+    about: string;
+    apporach: string;
+    facebook: string;
+    rating: number;
+    created: Date;
+    createdby: number;
+    main_image: URL;
+    lat: number;
+    lng: number;
+    views: number;
+    deleted: boolean;
+    big_image: URL;
+    small_image: URL;
+}
