89c354043853c5b1a5f09cd078db1d6d3f738aac
sberbom <sigmunbb@stud.ntnu.no>
2021-06-04 01:20:22 +0200

    manual cors

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ const app = express()
 app.use(cors({
   origin = '*'
 }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 const port = process.env.PORT || 3001
 
